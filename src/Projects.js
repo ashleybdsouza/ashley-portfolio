c2ece755be95e2cd7c4cf522eb2fc0c7109211be
Ashley DSouza <dsouzabash@gmail.com>
2024-11-22 11:32:28 -0800

    Adding the link to the eleventy project

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles/App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Projects() {
   return (
@@ -7,9 +9,13 @@ function Projects() {
       <h2>Projects</h2>
       <div className="project-list">
         <div className="project-card">
-          <h3>Project 1</h3>
-          <p>A description of your project goes here.</p>
-          <a href="https://github.com/yourprofile/project1" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+          <h3>Pages Using Eleventy</h3>
+          <p>
+            <a href="https://github.com/ashleybdsouza/tech-journey" target="_blank" rel="noopener noreferrer">Source Code <FontAwesomeIcon icon={faGithub} className="icon" /> </a>
+          </p>
+          <p>
+            <a href="https://ashleybdsouza.netlify.app/pages/portfolio-list/" target="_blank" rel="noopener noreferrer">Deployed Project on Netlify</a>
+          </p>
         </div>
         <div className="project-card">
           <h3>Project 2</h3>
